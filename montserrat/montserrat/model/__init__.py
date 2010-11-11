7b6f8d7decb3477594a01869a97b649c2f25a4c3
maguire <mcqlax31@gmail.com>
2010-11-11 18:46:50 -0500

    basic user creation/login

--- montserrat/montserrat/model/__init__.py
+++ montserrat/montserrat/model/__init__.py
@@ -1,6 +1,7 @@
 """The application's model objects"""
 from montserrat.model.meta import Session, Base
 
+from montserrat.model.user import User
 
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
